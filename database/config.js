47ace1e7cc3c477252d0551a1e72c405e02b6a17
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2023-01-16 18:31:18 +0530

    removed nodemon3

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 
-const user = process.env.USER || "admin";
-const password = process.env.PASS || "7CpGZ826n5qUXRtj";
+const user = "admin";
+const password = "7CpGZ826n5qUXRtj";
 
 
 
