ba37ea1d33b1575745673d0403b3f252b51d9b06
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2023-01-16 18:27:54 +0530

    removed nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routs/user-routs.js';
 import { conn } from './database/config.js';
 
-const cors = require("cors");
+// const cors = require("cors");
 const port = process.env.PORT || 80;
 const app = express();
 
@@ -14,7 +14,7 @@ var allowCrossDomain = function(req, res, next) {
 }
 
 app.use(allowCrossDomain);
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json());
 app.get("/",(req,res)=>{
