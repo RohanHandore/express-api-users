f0f052f70fd441a973bce34d73b8b98a6310c0ef
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2023-03-09 13:19:19 +0530

    added morgan to see logs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import router from './routs/user-routs.js';
 import { conn } from './database/config.js';
+import morgan from 'morgan'
+
 
 // const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(morgan(':method :url'));
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -17,7 +20,7 @@ app.use(allowCrossDomain);
 // app.use(cors())
 
 app.use(express.json());
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello world")
 })
 app.use("/api/user", router);
